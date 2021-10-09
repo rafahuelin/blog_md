077623dd59a432dde08b22c07b4fe8cbeb89eae4
rafahuelin <rafahuelin@gmail.com>
2021-10-09 13:07:41 +0200

    show Post model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'published', 'created', 'updated')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'body')
+    readonly_fields = ('created', 'updated')
+    list_per_page = 25
+
+
+admin.site.register(Post, PostAdmin)
