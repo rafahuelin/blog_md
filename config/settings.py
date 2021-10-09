9887c94f7c4879128ed0e13fbe164a02593841c1
rafahuelin <rafahuelin@gmail.com>
2021-10-09 15:06:45 +0200

    static and media url and dirs

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static_in_env']
+STATIC_ROOT = BASE_DIR / 'static/'
+VENV_PATH = BASE_DIR
+MEDIA_URL = '/media/'
+MEDIA_ROOT = VENV_PATH / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
