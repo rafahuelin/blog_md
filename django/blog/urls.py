26d9d69539f5e709c9eafd043cbcc44d995c4181
rafahuelin <rafahuelin@gmail.com>
2021-12-12 10:33:38 +0100

    #18 make whole url lowercase

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -4,7 +4,7 @@ from blog.views import PostListView
 
 
 urlpatterns = [
-    path('api/V1/posts/', PostListAPI.as_view()),
-    path('api/V1/posts/<int:pk>/', PostDetailAPI.as_view()),
+    path('api/v1/posts/', PostListAPI.as_view()),
+    path('api/v1/posts/<int:pk>/', PostDetailAPI.as_view()),
     path('posts/', PostListView.as_view(), name='posts_list'),
 ]
